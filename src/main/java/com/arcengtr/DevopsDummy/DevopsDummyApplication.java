2bac5d0adc04f3b32ee47587e096f4bbf8d23c8f
ArcenGTR <vecher.klasni@gmail.com>
2025-07-09 15:46:02 +0200

    initial commot

---
+++ src/main/java/com/arcengtr/DevopsDummy/DevopsDummyApplication.java
@@ -0,0 +1,13 @@
+package com.arcengtr.DevopsDummy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsDummyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsDummyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arcengtr/DevopsDummy/DevopsDummyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcengtr.DevopsDummy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsDummyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
